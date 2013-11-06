bfa0cf9173b52ad7b888783de127773836b7a65d
harrisonreid <harrison.reid.91@gmail.com>
2013-11-06 14:44:44 +1100

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatars/#{gravatar_id}.png"
+		image_tag(gravatar_url, alt: user.name, class: "gravatar")
+	end
+
+	def profile_pic_for(user)
+		image_tag("./rails.png")
+	end
+
 end
